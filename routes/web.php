b9f302cc3c299cd20c00f39e2389dde2fa00b755
gio-jac <giovanny.jacobo@gmail.com>
2023-11-10 17:06:58 -0600

    Diseño inicial login

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
+Route::get('/inertiaTest', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
@@ -25,7 +25,11 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
+Route::get('/login2', function () {
+    return Inertia::render('Auth/Login2');
+});
+
+Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
